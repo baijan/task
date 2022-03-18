63af06511122b4cee3ff7b032235e52787ce20aa
baijan <baijan.samud@gmail.com>
2022-03-18 19:28:11 +0600

    svg proba

---
+++ flutter_SingleChildScrollView/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter\\flutter_windows_2.10.0-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\flutter_22\\flutter_SingleChildScrollView" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter_windows_2.10.0-stable\\flutter"
+  "PROJECT_DIR=C:\\flutter_22\\flutter_SingleChildScrollView"
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter_windows_2.10.0-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\flutter_22\\flutter_SingleChildScrollView\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\flutter_22\\flutter_SingleChildScrollView"
+  "FLUTTER_TARGET=C:\\flutter_22\\flutter_SingleChildScrollView\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\flutter_22\\flutter_SingleChildScrollView\\.dart_tool\\package_config.json"
+)
